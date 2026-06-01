17c240d6958942682b2bcf2aae51d758700fbee9
Sukidama <sukidama@gmail.com>
2026-06-01 11:32:59 -0300

    first commitssss

---
+++ src/main/java/sukidama/sukidama/SukidamaDataGenerator.java
@@ -0,0 +1,11 @@
+package sukidama.sukidama;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SukidamaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/sukidama/sukidama/SukidamaModClient.java
@@ -0,0 +1,11 @@
+package sukidama.sukidama;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SukidamaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+
+}
